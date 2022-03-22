11bb8bb9b333c347941c3728487d800b5140151a
Vladimir Kurganov <err0rfrost@gmail.com>
2022-03-22 14:04:47 +0600

    Routes to links
    Add logic to switch between different pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Routes, BrowserRouter, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Altyn from './Altyn.js';
+import IDOhelp from './IDOhelp.js';
 
 function App() {
   return (
     <div className="App">
-        <Altyn/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Altyn/>} />
+          <Route exact path="/IDOhelp" element={<IDOhelp/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
